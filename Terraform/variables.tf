8bab848c7c468eb2dc60f15b90770fc1a7d35271
venkat-6666 <venkatg01234@gmail.com>
2025-11-15 15:16:16 +0530

    Update project variable default value

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "project" {default ="gowtham-project-476511"}
+variable "project" {default ="fifth-medley-478216-a7"}
 variable "region" { default = "us-central1" }
 variable "zone"   { default = "us-central1-a" }
 variable "ssh_user" { default = "ubuntu" }
