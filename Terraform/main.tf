9d6578be37b86ef275d85d90b9142519c267e525
venkat-6666 <venkatg01234@gmail.com>
2025-11-15 15:15:17 +0530

    Comment out region and zone in main.tf
    Comment out region and zone variables in provider block

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project = var.project
-  region  = var.region
-  zone    = var.zone
+  #region  = var.region
+  #zone    = var.zone
 }
 
 resource "google_compute_network" "vpc" {
