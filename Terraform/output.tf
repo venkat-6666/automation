fba8f0aa04ddd8a063e69c72fcd2f311394fdf38
venkat-6666 <venkatg01234@gmail.com>
2025-11-15 15:12:08 +0530

    Fix output to reference correct compute instance

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -4,7 +4,7 @@ output "manager_ip" {
 
 output "worker_ips" {
   value = [
-    for w in google_compute_instance.workers :
+    for w in google_compute_instance.worker :
     w.network_interface[0].access_config[0].nat_ip
   ]
 }
